6ddff21083562bab70afe4f5544e4641f7c070f1
ElsieWakarima <96049425+ElsieWakarima@users.noreply.github.com>
2022-05-31 12:48:03 +0300

    Add files via upload

---
+++ readingfile.java
@@ -0,0 +1,19 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class readingfile {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("art.txt")) {
+            int data = reader.read();
+            while (data != -1) {
+                System.out.print("" + "" + "  " + (char) data + "" + "" + "");
+                data = reader.read();
+            }
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
